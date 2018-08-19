f377fe63fbaa8126d2ab733c3a92a861bb204b5b
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-08-19 19:13:05 -0300

    correcion en el switch case d tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -9,25 +9,25 @@ class Tarjeta implements TarjetaInterface {
       // Esto  esta hecho bien a proposito  :P.
 		
 			switch ($monto) {
-		case '10':
+		case 10:
 		$this->$saldo = $monto;
 			return true;
-		case '20':
+		case 20:
 		$this->$saldo = $monto;
 			return true;
-		case '30':
+		case 30:
 		$this->$saldo = $monto;
 			return true;
-		case '50':
+		case 50:
 		$this->$saldo = $monto;
 			return true;
-		case '100':
+		case 100:
 		$this->$saldo = $monto;
 			return true;
-		case '510.15':
+		case 510.15:
 		$this->$saldo = $monto+81.93;
 			return true;
-		case '962.59':
+		case 962.59:
 		$this->$saldo = $monto+221.58;
 			return true;
 		default:
