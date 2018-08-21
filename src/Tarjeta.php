80c9fde573fb861d3f3dfdede27225346b97b7e0
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-08-20 23:47:25 -0300

    descuento de saldo implementado

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -3,41 +3,40 @@
 namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
-    protected $saldo;
+	protected $saldo;
+	
+	public function __construct($saldo) {
+        $this->saldo = $saldo;
+    }
 
     public function recargar($monto) {
       // Esto  esta hecho bien a proposito  :P.
-		
-			switch ($monto) {
-		case 10:
-		$this->$saldo = $monto;
-			return true;
-		case 20:
-		$this->$saldo = $monto;
-			return true;
-		case 30:
-		$this->$saldo = $monto;
-			return true;
-		case 50:
-		$this->$saldo = $monto;
-			return true;
-		case 100:
-		$this->$saldo = $monto;
-			return true;
-		case 510.15:
-		$this->$saldo = $monto+81.93;
-			return true;
-		case 962.59:
-		$this->$saldo = $monto+221.58;
-			return true;
-		default:
-		
+		switch ($monto) {
+			case 10:
+			$this->$saldo = $monto;
+				return true;
+			case 20:
+			$this->$saldo = $monto;
+				return true;
+			case 30:
+			$this->$saldo = $monto;
+				return true;
+			case 50:
+			$this->$saldo = $monto;
+				return true;
+			case 100:
+			$this->$saldo = $monto;
+				return true;
+			case 510.15:
+			$this->$saldo = $monto+81.93;
+				return true;
+			case 962.59:
+			$this->$saldo = $monto+221.58;
+				return true;
+			default:
 			return false;
-}
-
-
-    
-   }
+		}
+    }
 
     /**
      * Devuelve el saldo que le queda a la tarjeta.
@@ -46,6 +45,5 @@ class Tarjeta implements TarjetaInterface {
      */
     public function obtenerSaldo() {
       return $this->saldo;
-    }
-
+	}
 }
--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -6,7 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class ColectivoTest extends TestCase {
 
-    public function testAlgoUtil() {
+    public function testConSaldo() {
+        $colectivo = new Colectivo(NULL, NULL, NULL);
+        $valor = 14.80;
+        $tarjeta = new Tarjeta(15.0);
+
+        $boleto = new Boleto(14.80, $colectivo, $tarjeta);
+
+        $this->assertEquals($colectivo->pagarCon($tarjeta), $boleto);
+    }
+
+    public function testSinSaldo() {
+        $colectivo = new Colectivo(NULL, NULL, NULL);
+        $valor = 14.80;
+        $tarjeta = new Tarjeta(5.0);
+
+        $this->assertEquals($colectivo->pagarCon($tarjeta), False);
 
     }
 }
