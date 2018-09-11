4d88cbab125ca573d6ef58413816192fdf49f890
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-09-10 22:09:50 -0300

    correcion en reducirsaldo

--- src/Medioboleto.php
+++ src/Medioboleto.php
@@ -15,19 +15,12 @@ class Medioboleto extends  Tarjeta {
 		}
 		
 		//Si el ultimo viaje fue realizado el dia de la fecha
-		if($this->ultviaje['year'] == getdate( $this->tiempo->time() )['year'] && $this->ultviaje['mon'] == getdate( $this->tiempo->time() )['mon'] && $this->ultviaje['mday'] == getdate( $this->tiempo->time() )['mday']){
+		if(getdate( $this->ultviaje )['year'] == getdate( $this->tiempo->time() )['year'] && getdate( $this->ultviaje )['mon'] == getdate( $this->tiempo->time() )['mon'] && getdate( $this->ultviaje )['mday'] == getdate( $this->tiempo->time() )['mday']){
 			//Si fue realizado en la misma "hora"
-			if($this->ultviaje['hours'] == getdate( $this->tiempo->time() )['hours']){
-				if($this->ultviaje['minutes']+5 >= getdate( $this->tiempo->time() )['minutes'] ){
-					return false;
-				}
+			if($this->tiempo->time() - $this->ultviaje < 300 ) {
+				return false;
 			}
-			//Si hubo un cambio de hora entre los 2 viajes. Ej ultviaje a las 14:57 y se intenta viajar a las 15:01
-			elseif($this->ultviaje['hours'] == getdate( $this->tiempo->time() )['hours']-1 ){
-				if($this->ultviaje['minutes']+5 >= getdate( $this->tiempo->time() )['minutes']+60){
-					return false;
-				}
-			}
+
 			//Si ya viajo en el dia de la fecha, este viaje sera su ultimo a mitad de valor
 			$this->limitdia = TRUE;
 		}
@@ -66,7 +59,7 @@ class Medioboleto extends  Tarjeta {
 			return false;
 		}
 
-		$this->ultviaje = getdate( $this->tiempo->time() );
+		$this->ultviaje = $this->tiempo->time();
 
 		return true;
 	}
