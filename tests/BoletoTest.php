da97c10a07e5be69be1f70117435919cc38438f2
antelor <42157966+antelor@users.noreply.github.com>
2018-09-11 17:40:01 -0300

    Update BoletoTest.php
    agrego codigo al test de boleto

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -12,4 +12,27 @@ class BoletoTest extends TestCase {
 
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
+    
+    public function testObtenerValores() {
+        $tiempo = new TiempoFalso();
+        
+        $linea='Rojo';        
+        $empresa='Muni';
+        $numero='144';
+        
+        $colectivo = new Colectivo($linea, $empresa, $numero);
+        $tarjeta = new Tarjeta($tiempo);
+        $tarjeta->recargar(100.0);
+            
+        $boleto = $colectivo->pagarCon($tarjeta);
+        
+        $this->assertEquals($boleto->obtenerColectivo(), $colectivo);
+        $this->assertEquals($boleto->obtenersaldo(), 85.2);
+        $this->assertEquals($boleto->obtenertipodetarjeta(), 'Tarjeta');
+        $this->assertEquals($boleto->obteneriID(), $tarjeta->obtenerID() );
+        $this->assertEquals($boleto->viajesplus(), 0);
+        $this->assertEquals($boleto->abonadoenviajesplus(), 0);
+
+
+    }
 }
