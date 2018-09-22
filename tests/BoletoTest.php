487ee4b0459d6bf8b762b0c11e434a6271600a74
Regolo alesandro <regoloalesandro@gmail.com>
2018-09-22 19:20:17 -0300

    cambios en boleto.php

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -12,4 +12,29 @@ class BoletoTest extends TestCase {
 
         $this->assertEquals($boleto->obtenerValor(), $valor);
     }
+    public function testLineaLectivo() {
+        $linea= 420;
+        $colectivo = new Colectivo($linea, NULL, NULL);
+        $boleto = new Boleto(NULL, NULL, NULL, NULL, $colectivo ->linea(), NULL, NULL, NULL, NULL);
+
+        $this->assertEquals($boleto->obtenerLineadelcolectivo(), $linea);
+    }
+    public function testSaldo() {
+        $tiempo = new TiempoFalso();
+        $tarjeta = new Tarjeta( $tiempo );
+        $tarjeta->recargar(420);
+        $saldo=420;
+        $colectivo = new Colectivo($linea, NULL, NULL);
+        $boleto = new Boleto(NULL, NULL, $tarjeta, NULL, $colectivo ->linea(), NULL, NULL, NULL, NULL);
+
+        $this->assertEquals($boleto->obtenersaldo(), $saldo);
+    }
+    public function testfecha() {
+        $tiempo = new TiempoFalso();
+        $tarjeta = new Tarjeta( $tiempo );
+        $colectivo = new Colectivo($linea, NULL, NULL);
+        $boleto = new Boleto(NULL, NULL, $tarjeta, NULL, $colectivo ->linea(), NULL, NULL, NULL, $tiempo);
+
+        $this->assertEquals($boleto->obtenerfecha(), $tiempo);
+    }
 }
