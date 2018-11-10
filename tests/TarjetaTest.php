1ae9a8312ac0296c76e167051e85c4ba29d97641
Alesandro Regolo <regoloalesandro@gmail.com>
2018-11-10 11:07:19 -0300

    cambio en la logica de testtrasbordo

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -94,25 +94,21 @@ class TarjetaTest extends TestCase {
     }	
 	public function testtrasbordo(){
         $tarjeta = new Tarjeta(new TiempoFalso()); //Se crea el 1 de enero de 1970: Jueves 00:00hs
-        $tarjeta->recargar(100);
-        $tarjeta->pagarPasaje();
-        $tarjeta->recargar(100);
+		$colectivo = new Colectivo('a', 'a', 1);
+		$tarjeta->recargar(100);		
+		$colectivo->pagarCon($tarjeta);
         $this->assertEquals($tarjeta->obtenerSaldo(),85.2);
         $tarjeta->avanzarTiempo(5000);
-        $tarjeta->pagarPasaje();
-		$tarjeta->recargar(100);
+        $colectivo->pagarCon($tarjeta);
         $this->assertEquals($tarjeta->obtenerSaldo(),80.27);
         $tarjeta->avanzarTiempo(21200);
-        $tarjeta->pagarPasaje();
-		$tarjeta->recargar(100);
+        $colectivo->pagarCon($tarjeta);
         $this->assertEquals($tarjeta->obtenerSaldo(),65.47);
         $tarjeta->avanzarTiempo(4000);
-        $tarjeta->pagarPasaje();
-		$tarjeta->recargar(100);
+        $colectivo->pagarCon($tarjeta);
         $this->assertEquals($tarjeta->obtenerSaldo(),50.67);
         $tarjeta->avanzarTiempo(3000);
-        $tarjeta->pagarPasaje();
-		$tarjeta->recargar(100);
+        $colectivo->pagarCon($tarjeta);
         $this->assertEquals($tarjeta->obtenerSaldo(),45.74);
     }
 
